7c72a6f831806ce6ced2c62a2a0701346261377c
Geovana Augusta <geovanaag@hotmail.com>
2022-12-18 13:50:53 -0300

    test: (2) - verify individual report of product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,31 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+def mockedProduct():
+    return Product(
+        1,
+        "Borracha",
+        "Papelaria Solar",
+        "2021-07-04",
+        "2029-02-09",
+        "FR48",
+        "ao abrigo de luz solar",
+    )
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+
+    assert mockedProduct().__str__() == (
+        "O produto Borracha"
+        " fabricado em 2021-07-04"
+        " por Papelaria Solar com validade"
+        " até 2029-02-09"
+        " precisa ser armazenado ao abrigo de luz solar."
+    )
+
+
+# SOURCE 8
+# Exercícios - Prática - Dia 02
+# __repr__ ou __str__ -> Objeto em String <3
+# https://docs.python.org/3/library/functions.html#repr
+# https://pt.stackoverflow.com/questions/176464/qual-%C3%A9-a-diferen%C3%A7a-entre-str-e-repr
